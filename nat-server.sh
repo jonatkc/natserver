b905da42aa114e5b64511f6c4d03f5f4a7f25b2e
Jonathan <jkc5288@mavs.uta.edu>
2024-02-01 17:09:14 -0600

    updated gitl

--- nat-server.sh
+++ nat-server.sh
@@ -5,7 +5,7 @@ sudo apt update
 #install utils
 sudo apt install nala -y
 
-sudo nala install docker-compose curl wget neofetch ufw iperf3 neovim htop cifs-utils -y
+sudo nala install docker-compose curl wget neofetch ufw iperf3 neovim htop cifs-utils tmux -y
 
 #upgrade
 sudo nala upgrade -y
